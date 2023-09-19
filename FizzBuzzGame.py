040391ba8d1bf27a8455704a6d3cfbcfb192ee04
Erfan-Souri <145362814+Erfan-Souri@users.noreply.github.com>
2023-09-19 13:06:58 +0330

    Add files via upload

---
+++ FizzBuzzGame.py
@@ -0,0 +1,11 @@
+# you can change the range of number which you want your program to run
+# checking the first if statement is essential for the logic of this game
+for number in range(1, 101):
+    if number % 3 == 0 and number % 5 == 0:
+        print("FizzBuzz")
+    elif number % 3 == 0:
+        print("Fizz")
+    elif number % 5 == 0:
+        print("Buzz")
+    else:
+        print(number)
