8ec99d01e51496e3fcfbacad98ae988407442292
Erfan-Souri <145362814+Erfan-Souri@users.noreply.github.com>
2023-09-19 13:23:18 +0330

    Update FizzBuzzGame.py

--- FizzBuzzGame.py
+++ FizzBuzzGame.py
@@ -1,3 +1,4 @@
+#The python codes for the famous fizz buzz game based on a simple children games Wikipedia : https://en.wikipedia.org/wiki/Fizz_buzz
 # you can change the range of number which you want your program to run
 # checking the first if statement is essential for the logic of this game
 for number in range(1, 101):
